766313892f731b421abe6a9f0027abbf866af3a9
Aliaksandr Mitskevich <mitskevich.aliaksandr@gmail.com>
2025-08-08 12:44:47 +0300

    Add support for extracting glyphs from fonts missing GSUB table
    - Introduced test cases for handling fonts without GSUB table.
    - Added utility to ensure unique unicode entries in `convertToSvgFont`.
    - Replaced `console.error` with `console.warn` for missing GSUB logs.
    - Included `font-without-gsub.ttf` for testing purposes.

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -6,10 +6,11 @@ const importTargets = {
   local: createCachedImport(async () => import('../src')),
   dist: createCachedImport(async () => import('../dist')),
 }
-const resolve = (format: Format): string => path.resolve(__dirname, `../assets/font.${format}`)
+const resolveDefaultFont = (format: Format): string => path.resolve(__dirname, `../assets/font.${format}`)
 
-const ttfOriginalFont = fs.readFileSync(resolve('ttf'))
-const woff2OriginalFont = fs.readFileSync(resolve('woff2'))
+const ttfOriginalFont = fs.readFileSync(resolveDefaultFont('ttf'))
+const woff2OriginalFont = fs.readFileSync(resolveDefaultFont('woff2'))
+const ttfWithoutGSUB = fs.readFileSync(path.resolve(__dirname, '../assets/font-without-gsub.ttf'))
 const ABC_SVG_PATH = 'd="M448 -277L416 -277L416 -288L373 -288L373 -224L416 -224L416 -235L448 -235L448 -213Q448 -205 441.5 -198.5Q435 -192 427 -192L363 -192Q354 -192 347.5 -198.5Q341 -205 341 -213L341 -299Q341 -307 347.5 -313.5Q354 -320 363 -320L427 -320Q435 -320 441.5 -313.5Q448 -307 448 -299ZM171 -299L171 -192L139 -192L139 -224L96 -224L96 -192L64 -192L64 -299Q64 -307 70.5 -313.5Q77 -320 85 -320L149 -320Q158 -320 164.5 -313.5Q171 -307 171 -299ZM139 -288L96 -288L96 -256L139 -256ZM288 -256Q297 -256 303 -249.5Q309 -243 309 -235L309 -213Q309 -205 303 -198.5Q297 -192 288 -192L203 -192L203 -320L288 -320Q297 -320 303 -313.5Q309 -307 309 -299L309 -277Q309 -269 303 -262.5Q297 -256 288 -256ZM235 -288L235 -272L277 -272L277 -288ZM277 -240L235 -240L235 -224L277 -224Z"'
 
 const extract: Extract = async (...args: any[]): Promise<any> => {
@@ -56,7 +57,7 @@ describe('extract', () => {
   const createTestForRawFunctionality = (type: string, source: Buffer): void => {
     it(`should extract by raw unicode by ${type}`, async () => {
       const { meta } = await extract(
-        woff2OriginalFont,
+        source,
         { fontName: 'test-icons', raws: [''], formats: ['woff2'] },
       )
       expect(meta).toHaveLength(1)
@@ -71,4 +72,16 @@ describe('extract', () => {
     ['ttf', ttfOriginalFont],
     ['woff2', woff2OriginalFont],
   ].forEach(([type, source]) => { createTestForRawFunctionality(type.toString(), Buffer.from(source)) })
+
+  it('should correct extract by raw symbols from font without GSUB table', async () => {
+    const { meta } = await extract(
+      ttfWithoutGSUB,
+      { fontName: 'test-icons', raws: ['0'], formats: ['ttf'] },
+    )
+
+    expect(meta).toHaveLength(1)
+    const [glyphMeta] = meta
+    expect(glyphMeta.name).toEqual('0')
+    expect(glyphMeta.unicode).toEqual(['0'])
+  })
 })
